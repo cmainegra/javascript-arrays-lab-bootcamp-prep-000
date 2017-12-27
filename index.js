804471e34e3ad430425b5a481b7d9021c97f5c41
Charles Mainegra <cmainegra@gmail.com>
2017-12-27 20:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
-function
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
