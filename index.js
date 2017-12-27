3257b7a298d6212a412a07c2278e8e877cd8de44
Charles Mainegra <cmainegra@gmail.com>
2017-12-27 20:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function prependKitten(name) {
   return newKittens
 }
 function removeLastKitten() {
-  var newKittens = 
+  var newKittens = slice.kittens(0,
+  kittens.length - 1)
+  return newKittens
 }
