fd47df97efd7d8afa5670d36a92801bb6b6a0754
Charles Mainegra <cmainegra@gmail.com>
2017-12-27 20:36:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function appendKitten(name) {
   var newKittens = [...kittens, name]
   return newKittens
 }
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
